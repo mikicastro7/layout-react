e089c2032b6e0f17c4555584f48b5e3d2b829c37
Miquel Castro <castro-miquel@hotmail.com>
2021-03-09 19:21:48 +0100

    Se mejora el html y se quita css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 function App() {
   return (
-  <div className="vh-100 container d-flex flex-column">
-    <header className="row">
-      <h1>Título</h1>
+  <div class="p-0 container vh-100 flex-column d-flex">
+    <header class="text-white d-flex align-items-center">
+      <h1 class="m-0 font-weight-bold">Título</h1>
     </header>
-    <main className="row flex-grow-1">
-      <article className="col-md-4">
+    <main class="row m-0 flex-grow-1">
+      <article class="text-justify col-12 col-md-8">
         <h2>Titular de noticia</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam sint harum placeat ex corporis quam,
           alias
@@ -15,24 +15,24 @@ function App() {
           quasi
           modi delectus vero rem in porro, distinctio maiores illum quos aperiam amet esse!</p>
       </article>
-      <aside className="col-md-8">
+      <aside class="col-12 col-md-4">
         <h3>Categorías</h3>
-        <ul className="Texto">
+        <ul class="list-unstyled">
           <li>Noticias de deporte</li>
           <li>Noticias de ocio</li>
           <li>Noticias de política</li>
           <li>Noticias de espectáculos</li>
         </ul>
         <h3>Otras cosas</h3>
-        <ul className="Texto">
+        <ul class="list-unstyled">
           <li>Nosequé</li>
           <li>Nosecuánto</li>
           <li>Talycual</li>
         </ul>
       </aside>
     </main>
-    <footer className="row">
-      <p className="mx-auto">Texto de footer</p>
+    <footer class="text-center text-white">
+      <p class="m-0">Texto de footer</p>
     </footer>
   </div>
   );
